f4c95f7627d2bedbe77d3428cd8428a2aa65d52a
Waltteri <waltteri.westerholm@gmail.com>
2025-09-10 13:25:14 +0300

    feat: Implement Footer component and integrate it into the RootLayout; enhance Home page content and structure

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+<footer className="border-t pt-4 font-bold">
+          <p className="text-center text-sm text-gray-500" >
+            &copy; {new Date().getFullYear()} Ukkis Safaris. All rights reserved.
+          </p>
+        </footer>
+    );
+}
