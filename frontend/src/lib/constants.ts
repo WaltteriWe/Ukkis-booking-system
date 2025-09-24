340d24b73cc348f88395b1beb608030d90487599
waltteriwe <walttewe@metropolia.fi>
2025-09-24 10:08:03 +0300

    improved styling on frontpage cards and paddings

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,19 +1,18 @@
 // src/lib/routes.ts
 
-
 export const routes = {
-  home: '/',
-  categories: '/categories',
-  bookings: '/bookings',
-  contact: '/contact',
+  home: "/",
+  categories: "/categories",
+  bookings: "/bookings",
+  contact: "/contact",
   //TODO: add other routes here if needed
 };
 
 export const colors = {
-  primary: '#1E3A8A', // Indigo-900
-  secondary: '#FBBF24', // Amber-400
-  accent: '#4F46E5', // Indigo-600
-  background: '#F9FAFB', // Gray-100
-  text: '#111827', // Gray-900
-  
+  primary: "#1E3A8A", // Indigo-900
+  secondary: "#FBBF24", // Amber-400
+  accent: "#4F46E5", // Indigo-600
+  background: "#F9FAFB", // Gray-100
+  text: "#111827", // Gray-900
+  buttonColor: "#F59E0B", // Amber-500
 };
